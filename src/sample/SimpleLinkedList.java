b741b8fe7e64eabee932e9d2d3534423af4a5a56
Shiva Shinde <shivasai.sindhe@snhu.edu>
2015-09-08 03:46:02 -0400

    Create SimpleLinkedList.java

---
+++ src/sample/SimpleLinkedList.java
@@ -0,0 +1,81 @@
+class Link
+{
+	public int id;
+	public double d;
+	Link next;
+	Link first = null;
+	
+	public Link(int x, double y)
+	{
+		id = x;
+		d = y;
+	}
+	
+	public void insertFirst(int x, double y)
+	{
+		Link newLink = new Link(x, y);
+		newLink.next = first;
+		first = newLink;
+	}
+	
+	public Link remove()
+	{
+		Link temp = first;
+		first = first.next;
+		return first;
+	}
+	
+	public boolean isEmpty()
+	{
+		return first == null;
+	}
+	
+	void displayEle()
+	{
+		System.out.println("the integer is:" +id +"and" + "/n"  + "the double is:" +d);
+	}
+	
+	void displayList()
+	{
+		Link current = first;
+		while(!isEmpty())
+		{
+			if(current == null)
+			{
+				break;
+			}
+				else
+				{
+			current.displayEle();
+			current = current.next;
+				}
+			
+		}
+	}
+	
+	
+	
+}
+
+
+public class SimpleLinkedList {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Link obj = new Link(3, 5.5);
+		obj.insertFirst(10, 2.33);
+		obj.insertFirst(20, 4.33);
+		obj.insertFirst(30, 6.33);
+		obj.insertFirst(40, 8.33);
+		
+		obj.remove();
+		
+		obj.displayList();
+		
+		
+
+	}
+
+}
